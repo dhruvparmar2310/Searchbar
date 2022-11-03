40e03f99f690d2e57e47ead043b2683bf7cc0338
Dhruv Parmar <65495463+dhruvparmar2310@users.noreply.github.com>
2022-11-03 14:28:02 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './App.css'
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import SearchBar from './components/SearchBar'
+
+function App () {
+  return (
+    <div>
+      <SearchBar />
+    </div>
+  )
+}
+
+export default App
